6b30ade311656923eb2e56d294bb2d17ddd773a0
gkasianenko <g.kasianenko@yandex.ru>
2022-09-22 19:03:19 +0300

    Updates

---
+++ static/js/6.920c6ff8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-complete-guide"]=this["webpackJsonpreact-complete-guide"]||[]).push([[6],{55:function(e,t,c){"use strict";c.r(t);var n=c(1);t.default=function(){return Object(n.jsx)("div",{className:"centered",children:Object(n.jsx)("p",{children:"Page not found - 404"})})}}}]);
+//# sourceMappingURL=6.920c6ff8.chunk.js.map
